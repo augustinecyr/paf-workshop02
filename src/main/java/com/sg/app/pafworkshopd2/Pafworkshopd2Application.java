9cbba4270d69398bc6ecf26b3e095ab2752477ea
augustinecyr <augustinecyr@gmail.com>
2022-11-29 11:17:43 +0800

    first commit

---
+++ src/main/java/com/sg/app/pafworkshopd2/Pafworkshopd2Application.java
@@ -0,0 +1,13 @@
+package com.sg.app.pafworkshopd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pafworkshopd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pafworkshopd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/app/pafworkshopd2/Pafworkshopd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.app.pafworkshopd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pafworkshopd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
