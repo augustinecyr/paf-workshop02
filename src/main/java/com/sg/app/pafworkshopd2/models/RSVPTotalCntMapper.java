77ab6580b488db417bff837527e810567be8a37e
augustinecyr <augustinecyr@gmail.com>
2022-11-29 12:04:49 +0800

    lunch

---
+++ src/main/java/com/sg/app/pafworkshopd2/models/RSVPTotalCntMapper.java
@@ -0,0 +1,15 @@
+package com.sg.app.pafworkshopd2.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class RSVPTotalCntMapper implements RowMapper<RSVP> {
+    @Override
+    public RSVP mapRow(ResultSet rs, int rowNum) throws SQLException {
+        RSVP r = new RSVP();
+        r.setTotalCnt(rs.getInt("total"));
+        return r;
+    }
+}
---
+++ src/main/java/com/sg/app/pafworkshopd2/services/RSVPService.java
@@ -0,0 +1,36 @@
+package com.sg.app.pafworkshopd2.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sg.app.pafworkshopd2.models.RSVP;
+import com.sg.app.pafworkshopd2.repositories.RSVPRepository;
+
+@Service
+public class RSVPService {
+
+    @Autowired
+    private RSVPRepository rsvpRepo;
+
+    public List<RSVP> getAllRSVP(String q) {
+        return rsvpRepo.getAllRSVP(q);
+    }
+
+    public RSVP searchRSVPByName(String name) {
+        return rsvpRepo.searchRSVPByName(name);
+    }
+
+    public RSVP insertPurchaseOrder(final RSVP rsvp) {
+        return rsvpRepo.insertRSVP(rsvp);
+    }
+
+    public boolean updateRSVP(final RSVP rsvp) {
+        return rsvpRepo.updateRSVP(rsvp);
+    }
+
+    public Integer getTotalRSVP() {
+        return rsvpRepo.getTotalRSVP();
+    }
+}
